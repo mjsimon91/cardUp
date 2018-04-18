dc262d2ef46aa23a8bda8d6c230f4f48f53c1db6
mjsimon91 <mjsimon91@gmail.com>
2018-04-17 20:46:01 -0400

    Heroku resolution

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './Navbar.css';
+
+const Navbar = props => {
+  
+    return (
+      <nav className="nav">
+        <div className="container">
+          <div className="row">
+            <div className="right-align score">
+             Score: {props.score} Top Score: {props.topScore}
+            </div>
+          </div> 
+        </div>
+      </nav>
+    );
+  
+}
+
+
+
+export default Navbar;
